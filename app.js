14db5c434406abf8b54be7fd8c409a23ffc05105
Bhoomika. <bhoomikulal735@gmail.com>
2026-05-19 13:07:19 +0530

    second updated  setup of the game

--- app.js
+++ app.js
@@ -25,5 +25,22 @@ boxes.forEach((box) =>{
             turnO=true;
         }
         box.disabled=true;
+
+        checkWinner();
     })
 })
+
+const checkWinner=()=>{
+    for(let pattern of winPattern){
+        let pos1=boxes[pattern[0]].innerText;
+        let pos2=boxes[pattern[1]].innerText;
+        let pos3=boxes[pattern[2]].innerText;
+
+        if(pos1!=""&&pos2!=""&&pos3!=""){
+            if(pos1==pos2&&pos2==pos3)
+            {
+                console.log("Winner",pos1);
+            }
+        }
+    }
+}
